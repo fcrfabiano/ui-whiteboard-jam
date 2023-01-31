25ccd900367ed12405620a40f325fe55ac0d8b75
Fabiano César <fcfabiano.cesar@gmail.com>
2023-01-30 22:51:52 -0300

    feat: implement default edge

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-import ReactFlow, { Background, Controls, Node } from 'reactflow';
+import ReactFlow, {
+  addEdge,
+  Background,
+  Connection,
+  ConnectionMode,
+  Controls,
+  Node,
+  useEdgesState,
+  useNodesState,
+} from 'reactflow';
 import { zinc } from 'tailwindcss/colors';
 import 'reactflow/dist/style.css';
 
 import { Square } from './components/nodes/Square';
+import { useCallback } from 'react';
+import DefaultEdge from './components/edges/DefaultEdge';
 
 const NODE_TYPES = {
   square: Square,
+};
+
+const EDGE_TYPES = {
+  default: DefaultEdge,
 };
 
 const INITIAL_NODES = [
@@ -18,12 +33,42 @@ const INITIAL_NODES = [
     },
     data: {},
   },
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 1000,
+      y: 400,
+    },
+    data: {
+      label: 'Hello',
+    },
+  },
 ] satisfies Node[];
 
 function App() {
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES);
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges((prevEdges) => addEdge(connection, prevEdges));
+  }, []);
+
   return (
     <div className="w-screen h-screen">
-      <ReactFlow nodeTypes={NODE_TYPES} nodes={INITIAL_NODES}>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        edgeTypes={EDGE_TYPES}
+        nodes={nodes}
+        connectionMode={ConnectionMode.Loose}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onNodesChange={onNodesChange}
+        defaultEdgeOptions={{
+          type: 'default',
+        }}
+      >
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
